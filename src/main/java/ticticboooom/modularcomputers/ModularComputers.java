329582b58d2934e8cf57168c97a6aac666a966c5
KyleBCox <kylecoxforweb@gmail.com>
2020-06-11 21:34:40 +0100

    registering types in main class

--- src/main/java/ticticboooom/modularcomputers/ModularComputers.java
+++ src/main/java/ticticboooom/modularcomputers/ModularComputers.java
@@ -1,15 +1,28 @@
 package ticticboooom.modularcomputers;
 
+import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ticticboooom.modularcomputers.init.BlockTypes;
+import ticticboooom.modularcomputers.init.ContainerTypes;
+import ticticboooom.modularcomputers.init.TileEntityTypes;
 
 @Mod(ModularComputers.MOD_ID)
 public class ModularComputers {
     public static final String MOD_ID = "modularcomputers";
     public  static final Logger LOGGER = LogManager.getLogger();
     public ModularComputers() {
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ContainerTypes.CONTAINER_TYPES.register(eventBus);
+        TileEntityTypes.TILE_ENTITIES.register(eventBus);
+        BlockTypes.BLOCKS.register(eventBus);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
